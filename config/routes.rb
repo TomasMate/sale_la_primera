3e613ef8dd2336fdbb4609e8524bd0f4989104ca
TomasMate <82169863+TomasMate@users.noreply.github.com>
2021-04-20 11:01:13 +0200

    Added About Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'about', to: 'pages#about' #en la URL el /about solicita una accion GET, y con to: definimos que mande este accion 
+                                 # al controlador pages_controller (solo pongo pages y rails entiende que es ese) y a la 
+                                 # accion about del controlador (hay que definirla). Esta acción por defecto va a buscar la 
+                                 # pagina que se llame como la accion "about". Si no esta me tira error, tengo que crearla.
 end
