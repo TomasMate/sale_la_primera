c84454a69268018189397ad230ddfab711bdf6d9
TomasMate <82169863+TomasMate@users.noreply.github.com>
2021-05-07 09:27:59 +0200

    Add a new article through a form and save it to the database. Params hash.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,17 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        redirect_to @article  #Tambien puedo redirect_to articles_path (route del show de articulos). Para que rails detecte el id
+                                    # del ultimo objeto cargado "en memoria",le digo articles_path(@articles), o solo @articles y extrae su id
+    end
+
+
 end
