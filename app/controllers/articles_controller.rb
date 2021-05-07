bcfb25ad98517020b30bcf054e0dcbfef45044bb
TomasMate <82169863+TomasMate@users.noreply.github.com>
2021-05-07 10:24:50 +0200

    Added error-messaging (validation of object regarding its class/model) and notice of successfully saved using flash hash.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,14 +10,19 @@ class ArticlesController < ApplicationController
     end
 
     def new
-
+        @article = Article.new
     end
 
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
-        @article.save
-        redirect_to @article  #Tambien puedo redirect_to articles_path (route del show de articulos). Para que rails detecte el id
+        
+        if @article.save
+            flash[:notice] = "Article was saved successfully."
+            redirect_to @article  #Tambien puedo redirect_to articles_path (route del show de articulos). Para que rails detecte el id
                                     # del ultimo objeto cargado "en memoria",le digo articles_path(@articles), o solo @articles y extrae su id
+        else 
+           render 'new'
+        end        
     end
 
 
